8928e6a66bd4e4182696860975d3ead71865bf9a
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-12-08 11:33:08 -0700

    Add dropdown menu

--- components/Categories/Categories.js
+++ components/Categories/Categories.js
@@ -1,12 +1,26 @@
+import { useEffect, useState } from "react"
+
 const Categories = () => {
+  const [categories, setCategories] = useState([])
+
   useEffect(() => {
     fetch('https://opentdb.com/api_category.php')
       .then(response => response.json())
-      .then(data => console.log(data))
+      .then(data => setCategories(data.trivia_categories))
   }, [])
-  
+
+  const returnDropdown = () => {
+    if (categories.length) {
+      return categories.map(cat => {
+        return (
+          <option></option>
+        )
+      })
+    }
+  }
+
   return (
-    <div> cats</div>
+    <select>{returnDropdown()}</select>
   )
 }
 
