640505f869d0b1f84824ce4a760d03a21c080b2e
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-12-08 12:18:50 -0700

    fix: adjust tailwind configs

---
+++ my-project/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'accent-1': '#333',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+  ],
+}
