99c2e20965b709cdbab561575b81d0006a5a7902
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-12-10 10:40:19 -0700

    feat: dynamic api call to page

--- pages/[...category].js
+++ pages/[...category].js
@@ -4,12 +4,16 @@ import { useRouter } from "next/router"
 const NewThing = ({playerCategory, router, numberQuestions}) => { 
   const [userCategory, setUserCategory] = useState('')
   const [numQuestions, setNumQuestions] = useState(0)
+  const [triviaQuestions, setTriviaQuestions] = useState([])
   
   useEffect(() => {
     if (router.query.category) {    
       setUserCategory({name: router.query.category[0].split(',')[1], id: router.query.category[0].split(',')[0]})
       setNumQuestions(router.query.category[1])
     }
+    fetch(`https://opentdb.com/api.php?amount=${numQuestions}&category=${userCategory.id}&difficulty=easy&type=multiple`)
+      .then(response => response.json())
+      .then(data => setTriviaQuestions(data))
   }, [router])
 
   return (
