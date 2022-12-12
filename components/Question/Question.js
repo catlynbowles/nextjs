311994a2e6c15e6080db738ce1787ecdf650d94c
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-12-12 08:57:16 -0700

    format doc, update to include unescape function for string chars

--- components/Question/Question.js
+++ components/Question/Question.js
@@ -1,39 +1,45 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-const Question = ({currentQuestion, generateNewQuestion}) => {
-  const [responses, setResponses] = useState([])
+const Question = ({ currentQuestion, generateNewQuestion }) => {
+  const [responses, setResponses] = useState([]);
 
   useEffect(() => {
     if (currentQuestion) {
-      const randomIndex = Math.floor(Math.random() * 3)
-      let totalAnswers = currentQuestion.incorrect_answers
+      const randomIndex = Math.floor(Math.random() * 3);
+      let totalAnswers = currentQuestion.incorrect_answers;
       if (totalAnswers.length === 3) {
-        totalAnswers.splice(randomIndex, 0, currentQuestion.correct_answer)
+        totalAnswers.splice(randomIndex, 0, currentQuestion.correct_answer);
       }
-      setResponses(totalAnswers)
+      setResponses(totalAnswers);
     }
-  }, [currentQuestion])
-  
+  }, [currentQuestion]);
+
   const generateRadioButtons = () => {
-      return responses.map(resp => {
-        return (
-          <div>
-            <input type='radio' id={resp} key={resp} name='answer' value={resp}/>
-            <label for={resp}>{resp}</label>
-          </div>
-        )
-      })
+    return responses.map((resp) => {
+      return (
+        <div>
+          <input type="radio" id={resp} key={resp} name="answer" value={resp} />
+          <label for={resp}>{resp}</label>
+        </div>
+      );
+    });
+  };
+
+  const unescape = (string) => {
+    return new DOMParser()
+      .parseFromString(string, "text/html")
+      .querySelector("html").textContent;
   }
 
   return (
     <div>
-      <h2>{currentQuestion && currentQuestion.question}</h2>
-      <form>
-       {responses.length && generateRadioButtons()}
-      </form>
-      <button type='submit'  onClick={() => generateNewQuestion()}>Submit</button>
+      <h2>{currentQuestion && unescape(currentQuestion.question)}</h2>
+      <form>{responses.length && generateRadioButtons()}</form>
+      <button type="submit" onClick={() => generateNewQuestion()}>
+        Submit
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Question
+export default Question;
--- pages/[...category].js
+++ pages/[...category].js
@@ -1,38 +1,44 @@
-import { useEffect, useState } from "react"
-import Question from "../components/Question/Question"
+import { useEffect, useState } from "react";
+import Question from "../components/Question/Question";
 
-const NewThing = ({playerCategory, router, numberQuestions}) => { 
-  const [userCategory, setUserCategory] = useState('')
-  const [numQuestions, setNumQuestions] = useState(0)
-  const [triviaQuestions, setTriviaQuestions] = useState([])
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
-  
+const NewThing = ({ playerCategory, router, numberQuestions }) => {
+  const [userCategory, setUserCategory] = useState("");
+  const [numQuestions, setNumQuestions] = useState(0);
+  const [triviaQuestions, setTriviaQuestions] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
   useEffect(() => {
-    if (router.query.category) {    
-      setUserCategory({name: router.query.category[0].split(',')[1], id: router.query.category[0].split(',')[0]})
-      setNumQuestions(router.query.category[1])
+    if (router.query.category) {
+      setUserCategory({
+        name: router.query.category[0].split(",")[1],
+        id: router.query.category[0].split(",")[0],
+      });
+      setNumQuestions(router.query.category[1]);
     }
-  }, [router])
-  
+  }, [router]);
+
   useEffect(() => {
-    fetch(`https://opentdb.com/api.php?amount=${numQuestions}&category=${userCategory.id}&difficulty=easy&type=multiple`)
-      .then(response => response.json())
-      .then(data => setTriviaQuestions(data.results))
-  }, [userCategory])
+    fetch(
+      `https://opentdb.com/api.php?amount=${numQuestions}&category=${userCategory.id}&difficulty=easy&type=multiple`
+    )
+      .then((response) => response.json())
+      .then((data) => setTriviaQuestions(data.results));
+  }, [userCategory]);
 
   const generateNewQuestion = () => {
-    console.log(
-      'hi'
-    )
-    setCurrentQuestionIndex(currentQuestionIndex + 1) 
-    }
-    
-    return (
-      <div>
-      <h2>{userCategory.name}</h2>
-      <Question currentQuestion={triviaQuestions[currentQuestionIndex]} generateNewQuestion={generateNewQuestion}/>
-    </div>
-  )
-}
+    console.log("hi");
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+  };
 
-export default NewThing
+  return (
+    <div>
+      <h2>{userCategory.name}</h2>
+      <Question
+        currentQuestion={triviaQuestions[currentQuestionIndex]}
+        generateNewQuestion={generateNewQuestion}
+      />
+    </div>
+  );
+};
+
+export default NewThing;
